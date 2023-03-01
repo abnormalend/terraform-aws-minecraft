dc69112e053daba8bdbcbbcbdf889240f3a2121a
Brent Rogers <brentarogers@gmail.com>
2023-03-01 14:39:18 -0500

    managed policies

--- main.tf
+++ main.tf
@@ -72,10 +72,6 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_access_port" {
   tags              = { Name = "SSH Access" }
 }
 
-# resource "aws_iam_service_linked_role" "minecraft_server_role" {
-#   aws_service_name   = "ec2.amazonaws.com"
-# }
-
 resource "aws_iam_role" "minecraft_server_role" {
   name               = "minecraft_server_role"
   assume_role_policy = <<EOF
@@ -98,6 +94,24 @@ EOF
 resource "aws_iam_instance_profile" "minecraft_server_profile" {
   name = "server_profile"
   role = aws_iam_role.minecraft_server_role.name
+}
+
+data "aws_iam_policy" "CloudWatchAgentServerPolicy" {
+  name = "CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy" {
+  role = aws_iam_role.minecraft_server_role
+  policy_arn = data.aws_iam_policy.CloudWatchAgentServerPolicy.arn
+}
+
+data "aws_iam_policy" "AmazonSSMManagedInstanceCore" {
+  name = "AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role = aws_iam_role.minecraft_server_role
+  policy_arn = data.aws_iam_policy.AmazonSSMManagedInstanceCore.arn
 }
 
 resource "aws_instance" "minecraft_server" {
