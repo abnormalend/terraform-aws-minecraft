975354537ec33667bf8654c426ba99e79636c1c9
Brent Rogers <brentarogers@gmail.com>
2023-03-01 12:13:53 -0500

    add a security group

--- main.tf
+++ main.tf
@@ -5,7 +5,11 @@ provider "aws" {
   }
 }
 
-
+resource "aws_default_vpc" "default" {
+    tags = {
+        Name = "Default VPC"
+    }
+}
 
 # data "aws_availability_zones" "available" {
 #   state = "available"
@@ -30,11 +34,28 @@ data "aws_ami" "amzLinux" {
   }
 }
 
+resource "aws_security_group" "minecraft_security" {
+  name = "minecraft_security"
+  description = "Allow access for minecraft server port and ec2 anywhere"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "minecraft_port" {
+  security_group_id = aws_security_group.minecraft_security.id
+  description = "Allows whole word access to the minecraft server port"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 25565
+#   to_port     = 25565
+  ip_protocol = "tcp"
+}
+
 resource "aws_instance" "minecraft_server" {
   ami           = data.aws_ami.amzLinux.id
   instance_type = var.ec2_instance_type
+  security_groups = [ aws_security_group.minecraft_security.name ]
 
   tags = {
     Name = "minecraft_server"
   }
 }
+
