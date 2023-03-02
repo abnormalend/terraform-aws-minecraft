d6762ecc9b7dfab7b23f106e74df19e5b36aaad4
Brent Rogers <brentarogers@gmail.com>
2023-03-02 13:40:02 -0500

    rename the log groups

--- main.tf
+++ main.tf
@@ -49,11 +49,11 @@ output "instance_id" {
 }
 
 resource "aws_cloudwatch_log_group" "minecraft_log" {
-  name              = "minecraft.log"
+  name              = "minecraft_log"
   retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "minecraft_server_messages" {
-  name              = "/var/log/messages"
+  name              = "minecraft_messages"
   retention_in_days = 30
 }
