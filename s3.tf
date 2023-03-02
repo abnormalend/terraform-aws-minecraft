9c34a4116fc951d9f0401b826f7f2c82eeb0d14c
Brent Rogers <brentarogers@gmail.com>
2023-03-02 13:23:30 -0500

    save s3 arn to parameter store

--- s3.tf
+++ s3.tf
@@ -6,11 +6,23 @@ output "minecraft_files_s3_arn" {
   value = aws_s3_bucket.minecraft_files.arn
 }
 
+resource "aws_ssm_parameter" "minecraft-files-arn" {
+  name  = "minecraft-files-arn"
+  type  = "String"
+  value = aws_s3_bucket.minecraft_files.arn
+}
+
 resource "aws_s3_bucket" "minecraft_backups" {
   count         = var.s3_backup ? 1 : 0
   bucket_prefix = "minecraft-backups"
 }
 
 output "minecraft_backups_s3_arn" {
+  value = aws_s3_bucket.minecraft_backups[0].arn
+}
+
+resource "aws_ssm_parameter" "minecraft_backups-arn" {
+  name  = "minecraft_backups-arn"
+  type  = "String"
   value = aws_s3_bucket.minecraft_backups[0].arn
 }
