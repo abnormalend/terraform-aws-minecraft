00f92d16ce68906efdd87774c4939d0157aff803
Brent Rogers <brentarogers@gmail.com>
2023-03-02 13:18:15 -0500

    fixing bucket names

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "minecraft_files" {
-  bucket_prefix = "minecraft_files"
+  bucket_prefix = "minecraft-files"
 }
 
 output "minecraft_files_s3_arn" {
@@ -8,7 +8,7 @@ output "minecraft_files_s3_arn" {
 
 resource "aws_s3_bucket" "minecraft_backups" {
   count         = var.s3_backup ? 1 : 0
-  bucket_prefix = "minecraft_backups"
+  bucket_prefix = "minecraft-backups"
 }
 
 output "minecraft_backups_s3_arn" {
