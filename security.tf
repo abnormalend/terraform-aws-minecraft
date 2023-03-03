4af10eeca074ba4028f9c283629c7a8b391298f7
Brent Rogers <brentarogers@gmail.com>
2023-03-03 09:42:40 -0500

    fix outbound security

--- security.tf
+++ security.tf
@@ -10,6 +10,16 @@ resource "aws_security_group" "minecraft_security" {
   vpc_id      = aws_default_vpc.default.id
 }
 
+resource "aws_vpc_security_group_egress_rule" "all_out" {
+  security_group_id = aws_security_group.minecraft_security.id
+  description       = "Allows unrestricted outbound access"
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+  tags              = { Name = "outbound" }
+}
+
 resource "aws_vpc_security_group_ingress_rule" "minecraft_port" {
   security_group_id = aws_security_group.minecraft_security.id
   description       = "Allows whole word access to the minecraft server port"
