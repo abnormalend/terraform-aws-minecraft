42327cfc47431a21e9a244828ce923e2470f0889
Brent Rogers <brentarogers@gmail.com>
2023-03-03 08:50:45 -0500

    replace server when userdata changes

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "aws_instance" "minecraft_server" {
     s3_file_url   = "s3://${aws_s3_bucket.minecraft_files.bucket}/"
     s3_backup_url = "s3://${aws_s3_bucket.minecraft_backups[0].bucket}/"
   }
+  user_data_replace_on_change = true
   user_data = <<EOF
 #!/bin/bash
 touch ~/test_user_data
